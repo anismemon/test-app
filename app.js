b2ce4d22bb616630150fa4e51f1c462bd23fc50a
anismemon <58182830+anismemon@users.noreply.github.com>
2020-07-17 18:30:38 -0400

    more fiddling

--- app.js
+++ app.js
@@ -4,24 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-function rand(min, max) {
-  let randomNum = Math.random() * (max - min) + min;
-  return Math.floor(randomNum);
-}
-let randNum = rand(1, 2)
-console.log(randNum)
+// function rand(min, max) {
+//   let randomNum = Math.random() * (max - min) + min;
+//   return Math.floor(randomNum);
+// }
+// let number = rand(1, 2)
+// console.log(number)
 
-let indexRouter
+// let indexRouter
 
-if (randNum === 1) {
-  indexRouter = require('.routes/index')
-} else {
-  indexRouter = require ('.routes/hike')
-}
+// if (randNum === 1) {
+//   indexRouter = require('.routes/index')
+// } else {
+//   indexRouter = require ('.routes/hike')
+// }
 
 
 
-// var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 // var hike = require('./routes/hike');
 
